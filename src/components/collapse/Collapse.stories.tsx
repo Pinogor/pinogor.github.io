25b834d9a9b480ff09e9c7a660271ec8ff1fb899
pin <pin>
2024-09-08 19:52:17 +0300

    fix after review

--- src/components/collapse/Collapse.stories.tsx
+++ src/components/collapse/Collapse.stories.tsx
@@ -1,11 +1,10 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import Collapse from './Collapse';
 import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import Collapse from './Collapse';
 
 const meta: Meta<typeof Collapse> = {
   title: 'Components/Collapse',
   component: Collapse,
-  tags: ['autodocs'],
   argTypes: {
     opened: {
       control: 'boolean',
@@ -17,21 +16,19 @@ const meta: Meta<typeof Collapse> = {
 export default meta;
 
 export const Default: StoryObj<typeof Collapse> = {
-  render: (args) => (
-    <div>
-      <Collapse {...args}>
-        <div style={{ padding: '20px', border: '1px solid #ddd' }}>
-          <p>Collapse.</p>
-          <p>
-            Это компонент Collapse. Он принимает только opened и children. Когда opened: true, дочерние компоненты
-            сначала монтируются, а потом плавно появляются. В обратном случае наоборот, после скрытия дочерних
-            компонентов, они размонтируются.
-          </p>
-        </div>
-      </Collapse>
-    </div>
-  ),
   args: {
     opened: true,
   },
+  render: (args) => (
+    <div>
+      <h1>Collapse</h1>
+      <Collapse opened={args.opened}>
+        <p>
+          Это компонент Collapse. Он принимает только opened и children. Когда opened: true, дочерние компоненты сначала
+          монтируются, а потом плавно появляются. В обратном случае наоборот, после скрытия дочерних компонентов, они
+          размонтируются.
+        </p>
+      </Collapse>
+    </div>
+  ),
 };
--- src/components/collapse/Collapse.tsx
+++ src/components/collapse/Collapse.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useLayoutEffect } from 'react';
+import React from 'react';
 import './collapse.css';
 
 interface CollapseProps {
@@ -7,32 +7,9 @@ interface CollapseProps {
 }
 
 const Collapse: React.FC<CollapseProps> = ({ opened, children }) => {
-  const [shouldRender, setShouldRender] = useState(opened);
-  const contentRef = useRef<HTMLDivElement | null>(null);
-
-  useLayoutEffect(() => {
-    if (opened) {
-      setShouldRender(true);
-    }
-  }, [opened]);
-
-  const handleTransitionEnd = () => {
-    if (!opened) {
-      setShouldRender(false);
-    }
-  };
-
   return (
-    <div
-      ref={contentRef}
-      className={`collapse-content ${opened ? 'open' : 'closed'}`}
-      onTransitionEnd={handleTransitionEnd}
-    >
-      {shouldRender && (
-        <div className={`collapse-text ${opened ? 'fade-in' : 'fade-out'}`}>
-          {children}
-        </div>
-      )}
+    <div className={`collapse-content ${opened ? 'open' : 'closed'}`}>
+      <div className={`collapse-text ${opened ? 'fade-in' : 'fade-out'}`}>{children}</div>
     </div>
   );
 };
